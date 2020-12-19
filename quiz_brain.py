4d58f48cb045911599661181571db7bcd0223b58
SamroodAli <samroodali@live.com>
2020-12-20 00:04:28 +0530

    Add:QuizBrain model class

---
+++ quiz_brain.py
@@ -0,0 +1,10 @@
+class QuizBrain:
+
+    def __init__(self, questions_list):
+        self.questions_list = questions_list
+        self.question_number = 0
+
+    def next_question(self):
+        current_question = self.questions_list[self.question_number]
+        user_answer = input(f"Q{self.question_number}: {current_question.text}\n[ true or false ]").lower()
+        return current_question.answer == user_answer
