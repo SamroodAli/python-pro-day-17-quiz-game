17e9fe47f3322d71301e12171f1bd7eb95ee8782
SamroodAli <samroodali@live.com>
2020-12-20 00:22:31 +0530

    Add:check answer function

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+quizbrain = QuizBrain()
+
+while quizbrain.still_has_questions():
+    quizbrain.next_question()
