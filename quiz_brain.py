d4ed74e22986e368cb995c61807a86dfb11e2f16
SamroodAli <samroodali@live.com>
2020-12-20 00:06:49 +0530

    Fix:QuizBrain model class next question

--- quiz_brain.py
+++ quiz_brain.py
@@ -6,5 +6,6 @@ class QuizBrain:
 
     def next_question(self):
         current_question = self.questions_list[self.question_number]
+        self.question_number += 1
         user_answer = input(f"Q{self.question_number}: {current_question.text}\n[ true or false ]").lower()
         return current_question.answer == user_answer
